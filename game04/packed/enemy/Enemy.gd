3d06af7fd8ac865d7658371d13bfbe31fb3039c5
Vincent Nguyen <brotherflab@gmail.com>
2019-03-20 22:51:00 -0700

    Updating master branch

--- game04/packed/enemy/Enemy.gd
+++ game04/packed/enemy/Enemy.gd
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 extends RigidBody2D
 
 var player = null
@@ -20,19 +19,3 @@ func _process(delta):
 	#	# Update game logic here.
 	#	pass
 	set_pos(get_pos() + Vector2(0, player.speed * speed_factor * delta))
-||||||| merged common ancestors
-=======
-extends RigidBody2D
-var _player = null
-var _speed_factor = 0.5
-func _ready():
-    _player = get_node("../Player")
-    add_to_group("enemy")
-    set_process(true)
-func _process(delta):
-    set_position(get_position() + Vector2(0, _player.speed * _speed_factor * delta))
-func hit_by_player():
-    set_process(false)
-    set_mode(MODE_RIGID)
-    set_linear_velocity(Vector2(0,-_player.speed * _speed_factor))
->>>>>>> mainMenu
