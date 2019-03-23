12c3eb55a4bcdf8c3ada4331fe66a80957c90d7a
Miguel Menjivar <miguel@Miguels-MacBook-Pro.local>
2019-03-22 23:22:46 -0700

    updating files for wall collision

--- game04/packed/walls/wallRight.gd
+++ game04/packed/walls/wallRight.gd
@@ -1,13 +1,26 @@
 extends Sprite
 
+var _player = null
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_player = get_node("../Player")
+	add_to_group("block")
+	set_process(true)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func hit_by_player():
+    set_process(false)
+
+
+
+
+
+
