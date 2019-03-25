be36ee974db65311626787452e8f0d010a6dcdca
Miguel Menjivar <miguel@Miguels-MacBook-Pro.local>
2019-03-24 22:17:25 -0700

    updating the clone road, it doesn't look nice. But I can't seem to get multiple sprites to stitch together to make it seem smooth. The car might have to shrink just a bit in order to get the collisions in check

--- game04/misc_scripts/BG.gd
+++ game04/misc_scripts/BG.gd
@@ -2,7 +2,7 @@ extends Sprite
 
 var y = 0
 var speed_constant = 800 # Higher means faster y movement.
-onready var player = get_node("../KinematicBody2D") # road
+onready var player = get_node("../Player") # road
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -13,6 +13,5 @@ func _process(delta):
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
 	
-	#y -= player.speed * delta
 	y -= speed_constant * delta
 	set_region_rect(Rect2(0, y, 640, 960))
